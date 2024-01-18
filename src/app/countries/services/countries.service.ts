71c482ddbd0e0924fa6498c8b4ea15aefdd844cb
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-17 21:40:59 -0300

    Programación de la CountryPage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -3,7 +3,7 @@
  */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -14,6 +14,16 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor( private http: HttpClient ) { }
+
+  searchCountryByAplhaCode( code: string ):Observable<Country | null> {
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null),
+        catchError( error => of(null) )
+       );
+  }
 
   /**
    * Busca países por su capital.
