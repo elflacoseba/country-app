44e47cc5f0bf74593049628d6c3949680e512feb
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-17 22:03:28 -0300

    Muestro el detalle de un país.

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+    public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,12 +25,13 @@ export class CountryPageComponent implements OnInit {
       switchMap( ({ id }) => this.countriesService.searchCountryByAplhaCode( id ) )
     )
       .subscribe( country => {
+
         if (!country) {
           return this.router.navigateByUrl('');
         }
 
-        console.log('Tenemos un país.');
-        return;
+        return this.country = country;
+
       })
   }
 
