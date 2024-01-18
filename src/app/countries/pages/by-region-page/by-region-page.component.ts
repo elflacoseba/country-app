0e51b3af5d37cfbbb75ff0cfd31690cffb8b3948
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-18 15:34:23 -0300

    Agrego botones para las regiones.

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,21 +2,29 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
+
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService:CountriesService ) { }
 
-  searchByRegion( term: string):void {
+  searchByRegion( region: Region):void {
+
+    this.selectedRegion = region;
+
     this.isLoading = true;
 
-    this.countriesService.searchRegion( term )
+    this.countriesService.searchRegion( region )
       .subscribe( countries => {
         this.countries = countries;
         this.isLoading = false;
